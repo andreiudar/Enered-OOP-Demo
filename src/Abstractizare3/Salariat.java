7678ca02f8f2747c364f640f5b802a7ea5dc9f28
Alex Fierascu <ioan-alexandru.fierascu@externals.adidas.com>
2019-07-30 20:13:59 +0300

    oop demo - exemplu clasa abstracta

---
+++ src/Abstractizare3/Salariat.java
@@ -0,0 +1,12 @@
+package Abstractizare3;
+
+
+public class Salariat extends Angajat {
+  public Salariat(String name, int paymentPerHour) {
+    super(name, paymentPerHour);
+  }
+  @Override
+  public int calculateSalary() {
+    return getPaymentPerHour() * 8;
+  }
+}
